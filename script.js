d752690728851592e758a81e4085f27ef165be6a
Leonardo Soares <leonardoxp9d@gmail.com>
2023-12-27 19:44:07 -0300

    refactor: update in slider transition mode

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
+const totalRadioInputs = document.querySelectorAll('input[type="radio"]').length;
 let count = 1;
 document.getElementById("radio1").checked = true;
 
-setInterval( function(){
-    nextImage();
-}, 5000);
+// Iniciar o processo após a página ser carregada
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(nextImage, 5000);
+});
 
 function nextImage() {
-    count++;
-    
-    if( count > 4) {
-        count = 1;
+    for (let i = 1; i <= totalRadioInputs; i++) {
+        const radioElement = document.getElementById("radio"+i);
+
+        if (radioElement.checked) {
+            count = i % totalRadioInputs + 1;
+            break;
+        }
     }
 
     document.getElementById("radio"+count).checked = true;
+    setTimeout(nextImage, 5000);
 }
+
